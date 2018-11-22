777233b087e8fcbc6645dc3d00bbe583223fdaec
Unknown <codingbenjamin@gmail.com>
2018-11-22 22:52:35 +0000

    pickle 0.98

--- src/client/app.ts
+++ src/client/app.ts
@@ -8,5 +8,5 @@ window["app"] = new App (Samples, "app")
 
 module.hot.accept('../client/app/samples', () => {
     var latest = require ('../client/app/samples')
-    window["app"] = new App (latest.Samples.prototype.constructor, "app", undefined, true)
+    window["app"] = new App (latest.Samples.prototype.constructor, "app", {isVdomRendered: true})
 })
